a2f4d2c7cec5afb6f6c5d3ee7d9f6b5c67f14ddc
Claudio Benghi <claudio.benghi@gmail.com>
2015-08-20 01:53:36 +0200

    Syncing Nuget and framework version numbering approach.

--- Xbim.Properties/Properties/AssemblyInfo.cs
+++ Xbim.Properties/Properties/AssemblyInfo.cs
@@ -7,11 +7,18 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Xbim.Properties")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xbim.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+
+// taken from shared configuration in other solutions
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Lockley Consulting")]
+[assembly: AssemblyProduct("Xbim Library")]
+[assembly: AssemblyCopyright("Copyright © Lockley Consulting 2007-201")]
+[assembly: AssemblyTrademark("Xbim")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,16 +28,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d1bf59b4-aee8-4e20-b6ba-f9c9292d664c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
